6fcf75ccf76020a29ed35dda8c90076da3507dc4
igorminash <iminash@inbox.ru>
2022-11-26 09:52:17 +0300

    Добавлено решение задачи 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число А: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int number2 = int.Parse(Console.ReadLine()!);
+number2 = -number2;
+Console.WriteLine($"Число {number1} в степени числа {number2} = {GetDegree(number2)} ");
+GetDegree(number2);
+
+
+int GetDegree (int num2 ){
+    int Num = 1;
+    for ( int i = 1; i <= num2; i++){
+        Num = number1 * Num;
+    }
+return Num;
+
+}
+
