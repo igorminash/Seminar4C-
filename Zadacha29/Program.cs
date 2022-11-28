7e7bb1b851f9d599501499959ddafbfacce3c145
igorminash <iminash@inbox.ru>
2022-11-28 20:17:59 +0300

    Добавлено решение 29 задачи

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] array = new int [8];
+Massive(array);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int [] Massive (int [] arr){
+    for(int i=0; i < arr.Length; i++){
+    arr[i] = new Random().Next(0,11);
+    }
+    return arr;
+}  
